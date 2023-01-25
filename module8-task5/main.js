136de486609540eb350b31c65fdc6dbf475d59ac
romanpast <pastroman98@gmail.com>
2023-01-25 19:51:34 +0900

    5

--- module8-task5/main.js
+++ module8-task5/main.js
@@ -3,8 +3,6 @@ const pageNumNode = document.querySelector("#pageNum");
 const limitNode = document.querySelector("#limit");
 const resultNode = document.querySelector(".result")
 const form = document.querySelector(".form");
-const limit = limitNode.value;
-const pageNum = pageNumNode.value;
 
 const cardMaker = (arr) => {
     for (i = 0; i < arr.length; ++i) {
@@ -17,6 +15,27 @@ const cardMaker = (arr) => {
     }
 }
 
+const getData = (page, limit) => {
+    resultNode.innerHTML = "";
+    resultNode.className = "result";
+    fetch(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`)
+    .then((response) => {
+        const result = response.json();
+        return result
+    })
+    .then((data) => {
+        let contentArray = [];
+        for (i = 0; i < data.length; ++i) {
+            contentArray.push(data[i].download_url);
+        }
+        cardMaker(contentArray)
+        console.log("fetched content:", contentArray);
+        localStorage.setItem('myData', JSON.stringify(contentArray))
+    })
+}
+
+
+
 if (localStorage.myData) {
     resultNode.innerHTML = "";
     let contentArray = JSON.parse(localStorage.myData);
@@ -25,20 +44,20 @@ if (localStorage.myData) {
 }
 
 form.addEventListener("submit", function (e) {
-    fetch("https://picsum.photos/v2/list?page=1&limit=10")
-        .then((response) => {
-            const result = response.json();
-            return result
-        })
-        .then((data) => {
-            resultNode.innerHTML = "";
-            let contentArray = [];
-            for (i = 0; i < data.length; ++i) {
-                contentArray.push(data[i].download_url);
-            }
-            cardMaker(contentArray)
-            console.log("fetched content:", contentArray);
-            localStorage.setItem('myData', JSON.stringify(contentArray))
-        })
+    let limit = limitNode.value;
+    let pageNum = pageNumNode.value;
+    if (pageNum < 1 || pageNum > 10 || pageNum == NaN) {
+        resultNode.innerHTML = "Page number is out of range from 1 to 10";
+        resultNode.classList.add("warning");
+    } else if (limit < 1 || limit > 10 || limit == NaN) {
+        resultNode.innerHTML = "Limit is out of range from 1 to 10";
+        resultNode.classList.add("warning");
+    } else if ((limit < 1 || limit > 10 || limit == NaN) && (pageNum < 1 || pageNum > 10 || pageNum == NaN)) {
+        resultNode.innerHTML = "Page number and Limit are out of range from 1 to 10";
+        resultNode.classList.add("warning");
+    } else {
+        getData(pageNum, limit);
+    }
+    
     e.preventDefault();
 })
